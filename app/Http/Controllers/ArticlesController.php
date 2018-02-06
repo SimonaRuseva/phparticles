75c2440c95f24be6a99358e20b37cf68cab658a9
SimonaRuseva <ralito77@abv.bg>
2018-02-06 21:10:07 +0200

    update ArticlesController

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Autors;
+use App\Types;
 use Illuminate\Http\Request;
-
+use App\Articles;
+use Illuminate\Support\Facades\Validator;
 class ArticlesController extends Controller
 {
     /**
@@ -13,7 +16,10 @@ class ArticlesController extends Controller
      */
     public function index()
     {
-        //
+        $articles = Articles::with('types')->with('autors')->get();
+        return view('articles.index')->with('articles', $articles);
+//        $articles = articles::latest()->paginate(50);
+//        return view("articles.index", compact("articles"))->with('i', (request()->input('page', 1) - 1) * 50);
     }
 
     /**
@@ -23,7 +29,9 @@ class ArticlesController extends Controller
      */
     public function create()
     {
-        //
+        $types = Types::all();
+        $autors = Autors::all();
+        return view('articles.create')->with('types', $types)->with('autors', $autors);
     }
 
     /**
@@ -34,7 +42,25 @@ class ArticlesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $rules = array(
+            'title'       => 'required|min:4',
+        );
+        $validator = Validator::make($request->all(), $rules);
+        // process the login
+        if ($validator->fails()) {
+            return redirect('articles/create')
+                ->withErrors($validator)
+                ->withInput($request->all());
+        } else {
+            $subject = new Articles([
+                'type_id' => $request->get('type_id'),
+                'autor_id' => $request->get('autor_id'),
+                'title' => $request->get('title'),
+                'description' => $request->get('description'),
+            ]);
+            $subject->save();
+            return redirect('articles');
+        }
     }
 
     /**
@@ -45,7 +71,7 @@ class ArticlesController extends Controller
      */
     public function show($id)
     {
-        //
+
     }
 
     /**
@@ -56,7 +82,9 @@ class ArticlesController extends Controller
      */
     public function edit($id)
     {
-        //
+        $tutor = Articles::find($id);
+        return view('articles.edit', compact('tutor','id'));
+
     }
 
     /**
@@ -68,7 +96,10 @@ class ArticlesController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $articles = Articles::find($id);
+        $articles->title = $request->get('title');
+        $articles->save();
+        return redirect('/admin/articles')->with('success', 'Task was successful!');
     }
 
     /**
@@ -79,6 +110,8 @@ class ArticlesController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $sample = Articles::find($id);
+        $sample->delete();
+        return redirect('/articles')->with('success', 'Article was deleted!');
     }
 }
